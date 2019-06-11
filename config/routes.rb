28dd2173f7fce8ed046b63bf2c3cd75db3eae291
Leila Weiss <leila.weiss@powerhrg.com>
2019-06-10 20:51:04 -0400

    studio index

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
+  get 'studio/index'
+
 
   get "/signup", to: "users#new", as: "new_user"
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  root 'studio#index'
+
   resources :users, only: [:index, :create, :show]
 
   resources :spin_classes, only: [:index, :new, :create, :show] do
     resources :reservations, only: [:index, :new, :create, :show]
   end
-  resources :studios
+  resources :studios, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
