c9e2c3b618b7976879d2fb3ca10b027bac7bbd20
Leila Weiss <leila.weiss@powerhrg.com>
2019-06-10 20:03:29 -0400

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+SpinClass.destroy_all
+SpinClass.create(name: "Fusion Class")
+SpinClass.create(name: "Spin/Barre")
+SpinClass.create(name: "70s music mix")
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
