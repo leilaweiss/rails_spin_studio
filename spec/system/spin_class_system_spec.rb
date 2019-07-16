56c2886c05d9fb5a3794c2dd89584a070fe54c24
Leila Weiss <leila.weiss@powerhrg.com>
2019-07-16 09:58:43 -0400

    sytem test

---
+++ spec/system/spin_class_system_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "Spin Class System Spec", :type => :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it "creates a new spin class" do
+    visit "/spin_classes/new"
+
+    fill_in "Name", :with => "New Spin Class"
+    click_button "Create Class"
+
+  end
+end
