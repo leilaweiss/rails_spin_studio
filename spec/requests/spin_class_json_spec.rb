554ff4f8c878dde75fbb1261906ec5cb3002a0dc
Leila Weiss <leila.weiss@powerhrg.com>
2019-07-17 06:42:16 -0400

    more tests

--- spec/requests/spin_class_json_spec.rb
+++ spec/requests/spin_class_json_spec.rb
@@ -1,11 +1,13 @@
-# require "rails_helper"
+require "rails_helper"
+RSpec.describe "Sign In" do
+  it "Upon login, the user index page is rendered" do
+    user = create(:user)
 
-# RSpec.describe "Widget management", :type => :request do
-
-#   it "creates a Widget and redirects to the Widget's page" do
-#     headers = { "CONTENT_TYPE" => "application/json" }
-#     post "/widgets", :params => '{ "widget": { "name":"My Widget" } }', :headers => headers
-#     expect(response).to redirect_to(assigns(:widget))
-#   end
-
-# end
+    get "/signin"
+    expect(response).to render_template(:new)
+  end
+ it "does not render a different template" do
+    get "/spin_classes"
+    expect(response).to_not render_template(:show)
+  end
+end
